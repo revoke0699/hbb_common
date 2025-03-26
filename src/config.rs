38002ea2e90d472e5eae7a230874a8e8f5d1d2ba
wq <46583229+revoke0699@users.noreply.github.com>
2025-03-26 11:49:17 +0800

    更新

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["47.109.52.104"];
+pub const RS_PUB_KEY: &str = "7iVKv0xnHxjr9oFOuWITyCIMgiBXY8awSEjR74+yD08=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
